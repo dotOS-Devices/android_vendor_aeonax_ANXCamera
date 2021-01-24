a860c5856e81e96a045523730d657a8a0a3506d2
Marco Rizzo <marco.rizzo.00@outlook.com>
2021-01-24 10:23:07 +0100

    anx: get rid of ANXExtraPhotos, move everything to /system
    Signed-off-by: Marco Rizzo <marco.rizzo.00@outlook.com>

--- anx-vendor.mk
+++ anx-vendor.mk
@@ -2,15 +2,12 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/aeonax/ANXCamera
 
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/product/bin,$(TARGET_COPY_OUT_PRODUCT)/bin) \
-    $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/product/etc,$(TARGET_COPY_OUT_PRODUCT)/etc) \
-    $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/product/priv-app/ANXCamera/lib,$(TARGET_COPY_OUT_PRODUCT)/priv-app/ANXCamera/lib) \
-    $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/product/priv-app/ANXExtraPhoto/lib,$(TARGET_COPY_OUT_PRODUCT)/priv-app/ANXExtraPhoto/lib) \
-    $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/system/etc,$(TARGET_COPY_OUT_SYSTEM)/etc)
+    $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/system/bin,$(TARGET_COPY_OUT_SYSTEM)/bin) \
+    $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/system/etc,$(TARGET_COPY_OUT_SYSTEM)/etc) \
+    $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/system/priv-app/ANXCamera/lib,$(TARGET_COPY_OUT_SYSTEM)/priv-app/ANXCamera/lib)
 
 PRODUCT_PACKAGES += \
-    ANXCamera \
-    ANXExtraPhoto
+    ANXCamera
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.miui.notch=1
